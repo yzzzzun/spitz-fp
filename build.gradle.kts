b91eb6632c061363575b46900d1e6a0cad266e31
yzzzzun <yzzzzun@gmail.com>
2025-04-01 23:44:36 +0900

    feat: gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,14 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.http4k:http4k-core:5.33.0.1")
+    implementation("org.http4k:http4k-server-jetty:5.33.0.1")
 
     testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    testImplementation("org.http4k:http4k-client-jetty:5.33.0.1")
     testImplementation("com.ubertob.pesticide:pesticide-core:1.6.6")
     testImplementation("io.strikt:strikt-core:0.34.0")
     testImplementation(kotlin("test"))
